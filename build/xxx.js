3c1e59d1c2badb164a3ded4c45e6710225743bba
gaoxiaochen <gxcsoccer@126.com>
2013-06-17 22:13:58 +0800

    update

--- build/xxx.js
+++ build/xxx.js
@@ -1,6 +1,1 @@
-define("xxx", [ "./template/tpl.html" ], function(require) {
-    var tpl = require("./template/tpl.html");
-    console.log(tpl);
-});define('template/tpl.html', [], function() {
-	return '<ul>\n\t<li></li>\n\t<li></li>\n\t<li></li>\n\t<li></li>\n\t<li></li>\n</ul>';
-});
+define("xxx",["./template/tpl.html"],function(require){var e=require("./template/tpl.html");console.log(e)}),define("template/tpl.html",[],function(){return"<ul>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n</ul>"});
