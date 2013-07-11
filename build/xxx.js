af8e9ade8d36f162645a2921b9174b44a8a54a67
gaoxiaochen <gxcsoccer@126.com>
2013-07-11 23:07:21 +0800

    fix bug

--- build/xxx.js
+++ build/xxx.js
@@ -1,1 +1,1 @@
-define("0",["1"],function(require){var e=require("./template/tpl.html");console.log(e)}),define("1",[],function(){return"<ul>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n</ul>"});
+define("0",["1"],function(require){var e=require("1");console.log(e)}),define("1",[],function(){return"<ul>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n	<li></li>\n</ul>"});
